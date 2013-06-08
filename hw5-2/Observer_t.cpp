dc0c2a4e301fc96310979d7b0ba060edb1561162
okland <omri.klinger@gmail.com>
2013-06-08 21:14:05 +0300

    minor

---
+++ hw5-2/Observer_t.cpp
@@ -0,0 +1,9 @@
+//
+//  Observer_t.cpp
+//  hw5-2
+//
+//  Created by Omri Klinger on 6/8/13.
+//  Copyright (c) 2013 Omri Klinger. All rights reserved.
+//
+
+#include "Observer_t.h"
---
+++ hw5-2/Subject_t.cpp
@@ -0,0 +1,9 @@
+//
+//  Subject_t.cpp
+//  hw5-2
+//
+//  Created by Omri Klinger on 6/8/13.
+//  Copyright (c) 2013 Omri Klinger. All rights reserved.
+//
+
+#include "Subject_t.h"
