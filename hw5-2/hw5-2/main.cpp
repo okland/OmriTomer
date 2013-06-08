511eea559124c3501ecddd42360bc0faba8ec58d
Tomer <tomercor@gmail.com>
2013-06-08 21:21:31 +0300

    correct project

---
+++ hw5-2/hw5-2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hw5-2
+//
+//  Created by Tomer on 6/8/13.
+//  Copyright (c) 2013 Tomer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
